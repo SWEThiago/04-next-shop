81e85068d4daee2673b2dc447cbad7b830fc7ca5
Thiago Dev <thiagodevprogramador@gmail.com>
2022-12-30 19:31:45 -0300

    global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$green500',
+})
+
+export default function Home() {
+  return <Button>hello world</Button>
+}
